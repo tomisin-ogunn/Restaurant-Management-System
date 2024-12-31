1431ac2f78257f0afd5133309014dd5e530a1938
Oluwatomisin Ogunnusi <210194658@aston.ac.uk>
2024-12-31 14:22:13 +0000

    Developed Tables (Models)

--- Products/models.py
+++ Products/models.py
@@ -13,19 +13,22 @@ class Product(models.Model):
 
 #Model holding information of Food table, inherits properties from abstract 'Products' superclass
 class Food(Product):
+    foodID = models.CharField(max_length=20, unique=True, primary_key=True, default=1)
     image = models.ImageField(upload_to='food_images/', blank=True, null=True)
     duration = models.CharField(max_length=50)
     allergen = models.CharField(max_length=50)
     ingredients = models.CharField(max_length=100)
+    calories = models.PositiveIntegerField(default=1)
     
     def __str__(self):
         return self.name
     
 #Model holding information of Drink table, inherits properties from abstract 'Products' superclass
 class Drink(Product):
+    drinkID = models.CharField(max_length=20, unique=True, primary_key=True, default=1)
     image = models.ImageField(upload_to='drink_images/', blank=True, null=True)
     description = models.CharField(max_length=100)    
-    alcoholConc = models.CharField(max_length=100)    
+    alcoholConc = models.CharField(max_length=10)    
     
     def __str__(self):
         return self.name
@@ -33,13 +36,13 @@ class Drink(Product):
 
 #Model holding inventory (stock) information
 class Inventory(models.Model):
-    itemID = models.CharField(max_length=50, unique=True, primary_key=True)
+    itemID = models.CharField(max_length=20, unique=True, primary_key=True)
     description = models.CharField(max_length=50)
     status = models.CharField(max_length=20, choices=[('in_stock', 'In Stock'), ('low', 'Low'), ('out_of_stock', 'Out of Stock')])
     last_updated = models.CharField(max_length=50)
     updated_by = models.DateTimeField()
-    foodId = 'test'
-    drinkId = 'test'
+    foodID = models.ForeignKey(Food, on_delete=models.CASCADE)
+    drinkID = models.ForeignKey(Drink, on_delete=models.CASCADE)
 
 
 
